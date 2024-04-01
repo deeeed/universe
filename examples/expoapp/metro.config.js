ad63ef3873731a9716c96f8ddeec2833adad0398
abretonc7s <arthur.breton@consensys.net>
2024-04-01 15:35:41 +0800

    feat: fml

--- examples/expoapp/metro.config.js
+++ examples/expoapp/metro.config.js
@@ -1,15 +1,42 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const escape = require('escape-string-regexp');
+
 const path = require('path');
+
+const pak = require('../../packages/design-system/package.json');
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
 // This can be replaced with `find-yarn-workspace-root`
 const monorepoRoot = path.resolve(projectRoot, '../..');
+const designSystem = path.resolve(monorepoRoot, 'packages/design-system');
+
+const modules = [
+  'react-native-paper',
+  'react-native-safe-area-context',
+  'react-native-reanimated',
+  ...Object.keys({ ...pak.peerDependencies }),
+];
+
+
+const extraNodeModules = modules.reduce((acc, name) => {
+  acc[name] = path.join(__dirname, 'node_modules', name);
+  return acc;
+}, {});
+
+
+// Prevent metro from resolving duplicate packages
+const blacklistRE = exclusionList(
+  modules.map(
+    (m) =>
+      new RegExp(`^${escape(path.join(designSystem, 'node_modules', m))}\\/.*$`),
+  ),
+);
 
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [monorepoRoot];
@@ -18,5 +45,24 @@ config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
 ];
+
+config.resolver = {
+  ...config.resolver,
+  // extraNodeModules,
+  blacklistRE,
+  resolveRequest: (context, moduleName, platform) => {
+    // if (moduleName.startsWith('@siteed/design-system')) {
+    //   // Logic to resolve the module name to a file path...
+    //   // NOTE: Throw an error if there is no resolution.
+    //   return {
+    //     filePath: designSystem + '/src/index.ts',
+    //     type: 'sourceFile',
+    //   };
+    // } 
+
+    // Ensure you call the default resolver.
+    return context.resolveRequest(context, moduleName, platform);
+  },
+};
 
 module.exports = config;
--- examples/expoapp/src/app/_layout.tsx
+++ examples/expoapp/src/app/_layout.tsx
@@ -1,10 +1,30 @@
 import { Slot } from 'expo-router';
 import { Text, View } from 'react-native';
 import { UIProvider } from '@siteed/design-system'
+import { LoggerProvider, useLoggerActions } from '@siteed/react-native-logger';
+import { useEffect } from 'react';
+import { useScreenWidth } from '@siteed/design-system';
+
+const WithLogger = ({ children }: { children: React.ReactNode }) => {
+    const { logger } = useLoggerActions('GoodApp')
+    const width = useScreenWidth()
+
+    useEffect(() => {
+        logger.info('App started')
+    }, [logger])
+
+    return (
+    <View>
+        <Text>Width: {width}</Text>
+        {children}
+        </View>
+    )
+}
 
 export default function HomeLayout() {
-  return <UIProvider>
-    <Text>Header</Text>
-    <Slot />
-    </UIProvider>;
+  return <LoggerProvider>
+        <WithLogger>
+            <Slot />
+        </WithLogger>
+    </LoggerProvider>;
 }
