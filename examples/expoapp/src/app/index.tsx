89e14bc4d1327dcf75bd3fd08de5dc89c6592671
abretonc7s <arthur.breton@consensys.net>
2024-04-03 10:09:54 +0800

    feat: migrate to latest yarn

--- examples/expoapp/src/app/index.tsx
+++ examples/expoapp/src/app/index.tsx
@@ -1,9 +1,11 @@
-import { LockInput } from '@siteed/design-system';
-import { Text, View } from 'react-native';
+import { LockInput } from "@siteed/design-system";
+import { Text, View } from "react-native";
 
 export default function Page() {
-  return <View>
-    <Text>Home page</Text>
-    <LockInput text='ok' locked />
-    </View>;
+  return (
+    <View>
+      <Text>Home page</Text>
+      <LockInput text="ok" locked />
+    </View>
+  );
 }
