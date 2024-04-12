76380c09f818cda031311b98a8446f1a852647b8
abretonc7s <arthur.breton@consensys.net>
2024-04-12 14:55:05 +0800

    feat: init utils project

---
+++ packages/utils/src/size.utils.ts
@@ -0,0 +1,18 @@
+/**
+ * Formats bytes into a human-readable format.
+ *
+ * @param bytes - The size in bytes.
+ * @param decimals - The number of decimal places to include in the result.
+ * @returns A formatted string representing the human-readable file size.
+ */
+export function formatBytes(bytes: number, decimals: number = 2): string {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
