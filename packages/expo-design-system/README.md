6bad120fa2ab85722334f2a264ef2ca9fef2cccb
abretonc7s <arthur.breton@consensys.net>
2024-04-28 09:09:37 +0800

    feat: cleanup

--- packages/expo-design-system/README.md
+++
@@ -1,3 +0,0 @@
-# @siteed/expo-design-system
-
-Auto configuration of the design system for expo
--- packages/expo-design-system/android/src/main/java/net/siteed/expodesignsystem/SiteedExpoDesignSystemView.kt
+++
@@ -1,7 +0,0 @@
-package net.siteed.expodesignsystem
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class SiteedExpoDesignSystemView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
