2475b8f94cce3dc758f898f8215b1f63c95d147a
abretonc7s <arthur.breton@consensys.net>
2024-07-29 10:27:28 +0800

    chore: Update design-system preview with lazy loading for images

--- packages/design-system/.storybook/preview.tsx
+++ packages/design-system/.storybook/preview.tsx
@@ -1,11 +1,10 @@
-import { LoggerProvider } from '@siteed/react-native-logger';
 import type { Decorator, Preview } from '@storybook/react';
 import React from 'react';
 import { Platform, ScrollView } from 'react-native';
+import { colorOptions } from '../src/_mocks/mock_data';
 import { ScreenWrapper } from '../src/components/screen-wrapper/screen-wrapper';
 import { UIProvider } from '../src/providers/ui-provider';
 import { ThemeConfig } from '../src/settings/theme-config/theme-config';
-import { colorOptions } from '../src/_mocks/mock_data';
 
 /** @type { import('@storybook/react').Preview } */
 const preview: Preview = {
@@ -26,33 +25,31 @@ export const decorators: Decorator[] = [
   (StoryFn) => {
     console.log('preview init decorators');
     return (
-      <LoggerProvider>
-        <>
-          {Platform.OS === 'web' ? (
-            <style type="text/css">
-              {`
+      <>
+        {Platform.OS === 'web' ? (
+          <style type="text/css">
+            {`
                   @font-face {
                     font-family: 'MaterialCommunityIcons';
                     src: url(${require('react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf')}) format('truetype');
                   }
                 `}
-            </style>
-          ) : null}
-          <UIProvider locale={'en'}>
-            <>
-              <ScrollView
-                horizontal={true}
-                showsHorizontalScrollIndicator={false}
-              >
-                <ThemeConfig flexDirection={'row'} colors={colors} />
-              </ScrollView>
-              <ScreenWrapper style={{ minHeight: 300, padding: 8 }}>
-                <StoryFn />
-              </ScreenWrapper>
-            </>
-          </UIProvider>
-        </>
-      </LoggerProvider>
+          </style>
+        ) : null}
+        <UIProvider locale={'en'}>
+          <>
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              <ThemeConfig flexDirection={'row'} colors={colors} />
+            </ScrollView>
+            <ScreenWrapper style={{ minHeight: 300, padding: 8 }}>
+              <StoryFn />
+            </ScreenWrapper>
+          </>
+        </UIProvider>
+      </>
     );
   },
 ];
