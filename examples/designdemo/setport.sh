f044951c7db8c64f5c7288a170ff0d616bf8637a
abretonc7s <arthur.breton@consensys.net>
2024-11-26 21:15:38 +0800

    chore: modify setport.sh script and update dependencies
    Changes made to setport.sh script and updates to dependencies in package.json and yarn.lock.

--- examples/designdemo/setport.sh
+++ examples/designdemo/setport.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Enable debug mode
-set -x
-
 CUSTOM_PORT=7765
 CURRENT_FOLDER=$(dirname "$0")
 
@@ -13,38 +10,40 @@ NC='\033[0m' # No Color
 
 echo -e "${GREEN}Setting Metro port to: $CUSTOM_PORT${NC}"
 
-# Function to update file if it exists and is regular
-function update_file() {
-    local file=$1
-    local search=$2
-    local replace=$3
-    
-    if [ -f "$file" ] && [ ! -h "$file" ]; then
-        # Use different sed syntax for different file types
+# Array of files to update
+FILES=(
+    "$CURRENT_FOLDER/ios/Pods/Headers/Public/React-Core/React/RCTDefines.h"
+    "$CURRENT_FOLDER/ios/Pods/Headers/Private/React-Core/React/RCTDefines.h"
+    "$CURRENT_FOLDER/node_modules/@react-native/gradle-plugin/react-native-gradle-plugin/src/main/kotlin/com/facebook/react/utils/AgpConfiguratorUtils.kt"
+    "$CURRENT_FOLDER/node_modules/@siteed/expo-audio-stream/node_modules/react-native/React/DevSupport/RCTInspectorDevServerHelper.mm"
+)
+
+for file in "${FILES[@]}"; do
+    if [ -h "$file" ]; then
+        # If it's a symlink, get the actual file
+        actual_file=$(readlink -f "$file")
+        echo -e "${YELLOW}Following symlink: $file -> $actual_file${NC}"
+        if [ -f "$actual_file" ]; then
+            if [[ $file == *".kt" ]]; then
+                sed -i '' "s/DEFAULT_DEV_SERVER_PORT = \"8081\"/DEFAULT_DEV_SERVER_PORT = \"$CUSTOM_PORT\"/" "$actual_file"
+            elif [[ $file == *"RCTInspectorDevServerHelper.mm" ]]; then
+                sed -i '' "s/@8081/@$CUSTOM_PORT/" "$actual_file"
+            else
+                sed -i '' "s/RCT_METRO_PORT 8081/RCT_METRO_PORT $CUSTOM_PORT/" "$actual_file"
+            fi
+        fi
+    elif [ -f "$file" ]; then
+        # Regular file
         if [[ $file == *".kt" ]]; then
-            # For Kotlin files, escape the quotes
             sed -i '' "s/DEFAULT_DEV_SERVER_PORT = \"8081\"/DEFAULT_DEV_SERVER_PORT = \"$CUSTOM_PORT\"/" "$file"
+        elif [[ $file == *"RCTInspectorDevServerHelper.mm" ]]; then
+            sed -i '' "s/@8081/@$CUSTOM_PORT/" "$file"
         else
-            # For header files
             sed -i '' "s/RCT_METRO_PORT 8081/RCT_METRO_PORT $CUSTOM_PORT/" "$file"
         fi
     else
-        echo -e "${YELLOW}Skipping: $file (not a regular file)${NC}"
+        echo -e "${YELLOW}File not found: $file${NC}"
     fi
-}
-
-# Update React Core files
-update_file "$CURRENT_FOLDER/ios/Pods/Headers/Public/React-Core/React/RCTDefines.h" \
-    "RCT_METRO_PORT 8081" \
-    "RCT_METRO_PORT $CUSTOM_PORT"
-
-update_file "$CURRENT_FOLDER/ios/Pods/Headers/Private/React-Core/React/RCTDefines.h" \
-    "RCT_METRO_PORT 8081" \
-    "RCT_METRO_PORT $CUSTOM_PORT"
-
-# Update Kotlin file
-update_file "$CURRENT_FOLDER/node_modules/@react-native/gradle-plugin/react-native-gradle-plugin/src/main/kotlin/com/facebook/react/utils/AgpConfiguratorUtils.kt" \
-    'DEFAULT_DEV_SERVER_PORT = "8081"' \
-    "DEFAULT_DEV_SERVER_PORT = \"$CUSTOM_PORT\""
+done
 
 echo -e "${GREEN}Port update completed${NC}"
